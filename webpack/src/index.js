1b97410564414db49afeb57d883eb3a1f859df88
awe <vitalikarach95@gmail.com>
2022-02-26 21:02:26 +0300

    configs for dev mode and prod mode

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,6 +3,10 @@ import './style.css';
 import Icon from './icon.png';
 import printMe from './print.js';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
